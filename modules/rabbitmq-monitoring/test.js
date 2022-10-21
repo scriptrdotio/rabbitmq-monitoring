c4b74031bda8e087cebbcd1767f626e846557aa7
walid <walid.bakkar@elementn.com>
2022-10-21 15:38:33 +0300

    Delete test.js

--- modules/rabbitmq-monitoring/test.js
+++
@@ -1,4 +0,0 @@
-var clientModule = require("modules/rabbitmq-monitoring/main.js");
-var rabbitmqMonitoring = new clientModule.RabbitMQMonitoringClient();
-//return rabbitmqMonitoring.getOverview();
-return rabbitmqMonitoring.getStats();
